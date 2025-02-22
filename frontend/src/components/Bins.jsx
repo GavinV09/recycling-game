a92c9e3da93f2c0e89a23a1b1ad4bae68e28a2a2
Gavin <gavinvernier@gmail.com>
2025-02-22 14:45:06 -0500

    fourth commit

--- frontend/src/components/Bins.jsx
+++ frontend/src/components/Bins.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import { useDrop } from 'react-dnd';
-import '../App.css';
+import React from "react";
+import { useDrop } from "react-dnd";
+import "../App.css";
 
 function Bins({ onDropItem }) {
   const [{ isOver }, drop] = useDrop(() => ({
-    accept: 'ITEM',
-    drop: (item) => onDropItem(item.type, 'bin-type'), // Replace 'bin-type' with actual bin type
+    accept: "ITEM",
+    drop: (item, monitor) => {
+      const binType = monitor.getItem().type; // Get the bin type from the item
+      onDropItem(item.id, item.type, binType);
+    },
     collect: (monitor) => ({
       isOver: !!monitor.isOver(),
     }),
-  }))
+  }));
 
   return (
     <div className="bins" ref={drop}>
-      <div className="bin-container recycling" onClick={() => onDropItem('recycling', 'recycling')}>
-        <div className="label">Recycling</div>
-        <img src="bins/recycle_bin.png" alt="Recycling Bin" className="bin recycling-bin" />
+      <div className="bin recycling-bin" data-bin-type="recycling">
+        <img
+          src="/bins/recycle_bin.png"
+          alt="Recycling Bin"
+          className="bin-image"
+        />
       </div>
-      <div className="bin-container compost" onClick={() => onDropItem('compost', 'compost')}>
-        <div className="label">Compost</div>
-        <img src="bins/compost_bin.png" alt="Compost Bin" className="bin compost-bin" />
+      <div className="bin trash-bin" data-bin-type="trash">
+        <img src="/bins/trash_bin.png" alt="Trash Bin" className="bin-image" />
       </div>
-      <div className="bin-container trash" onClick={() => onDropItem('trash', 'trash')}>
-        <div className="label">Trash</div>
-        <img src="bins/trash_bin.png" alt="Trash Bin" className="bin trash-bin" />
+      <div className="bin compost-bin" data-bin-type="compost">
+        <img
+          src="/bins/compost_bin.png"
+          alt="Compost Bin"
+          className="bin-image"
+        />
       </div>
     </div>
   );
